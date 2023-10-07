81946f73a9586daf2ae2166310e750ce9123f344
Azhar <azmd801@gmail.com>
2023-10-07 14:13:04 +0530

    environment variable and setup added

---
+++ source/configuration/mongo_db_connection.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+import certifi
+import pymongo
+
+from source.constants.database import DATABASE_NAME
+from source.constants.env_variable import MONGODB_URL_KEY
+from source.exception import SensorException
+
+ca = certifi.where()
+
+
+class MongoDBClient:
+    client = None
+
+    def __init__(self, database_name=DATABASE_NAME) -> None:
+        try:
+            if MongoDBClient.client is None:
+                mongo_db_url = os.getenv(MONGODB_URL_KEY)
+                if mongo_db_url is None:
+                    raise Exception(f"Environment key: {MONGODB_URL_KEY} is not set.")
+
+                MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+
+            self.client = MongoDBClient.client
+
+            self.database = self.client[database_name]
+
+            self.database_name = database_name
+
+        except Exception as e:
+            
+            raise SensorException(e, sys)

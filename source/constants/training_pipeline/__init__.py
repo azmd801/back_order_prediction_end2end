a9880cc2eaf5a56ad1b46aa84531ec2ff41dd5ed
Azhar <azmd801@gmail.com>
2023-10-09 12:43:29 +0530

    TrainingPipelineCongig and DataIngestionConfig added

--- source/constants/training_pipeline/__init__.py
+++ source/constants/training_pipeline/__init__.py
@@ -11,7 +11,7 @@ ARTIFACT_DIR: str = "artifact"
 
 # common file name
 
-FILE_NAME: str = "sensor.csv"
+FILE_NAME: str = "back_order.csv"
 
 TRAIN_FILE_NAME: str = "train.csv"
 
@@ -36,7 +36,7 @@ DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 
-DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
 
 """
 Data Validation realted contant start with DATA_VALIDATION VAR NAME
